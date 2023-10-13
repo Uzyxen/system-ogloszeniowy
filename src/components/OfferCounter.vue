6f75a70f10c7b1b4687f4521e38ad8c7849f4bdb
Uzyxen <michal912456@gmail.com>
2023-10-13 21:18:10 +0200

    Offer counter

---
+++ src/components/OfferCounter.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{ counter.toLocaleString() }}</h1>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                counter: Math.floor(Math.random() * (100000 - 80000 + 1)) + 80000
+            }
+        }
+    }
+
+</script>
+
+<style>
+    h1{
+        font-weight: 900;
+        font-size: 60px;
+        color: #6244DB;
+    }
+</style>
