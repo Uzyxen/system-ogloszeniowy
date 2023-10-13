b3259a07f6e4cf71964e1dc069f4c2abf3cefc81
Uzyxen <michal912456@gmail.com>
2023-10-13 21:09:48 +0200

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-<script>
-
-</script>
-
 <template>
-  <div>
-    <h2>Hello World</h2>
-  </div>
+  <TopBar/>
+  <HomePage/>
 </template>
+
+<script>
+  import TopBar from  './components/TopBar.vue';
+  import HomePage from './components/HomePage.vue';
+
+  export default {
+    components: {
+      TopBar,
+      HomePage
+    }
+  }
+</script>
 
 <style>
 
