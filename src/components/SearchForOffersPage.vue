1359d08d4e924cc38ae223bed823f7fac55afb3f
Uzyxen <michal912456@gmail.com>
2023-10-14 15:13:34 +0200

    search for offers form

--- src/components/SearchForOffersPage.vue
+++ src/components/SearchForOffersPage.vue
@@ -1,6 +1,20 @@
 <template>
     <article>
         <h2>Szukanie ofert</h2>
+        <div id="search-box">
+            <form action="">
+                <input type="text">
+                <select name="" id="">
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+                <input type="text">
+                <select name="" id="">
+                    <option value=""></option>
+                    <option value=""></option>
+                </select>
+            </form>
+        </div>
     </article>
 </template>
 
@@ -19,6 +33,15 @@
         color: #6244DB;
         font-size: 30px;
         font-weight: 700;
+    }
+
+    #search-box{
+        margin: 40px auto 0;
+        width: 1150px;
+        height: 300px;
+        border-radius: 5px;
+        background: #FFF;
+        box-shadow: 3px 4px 37px -3px rgba(0, 0, 0, 0.25);
     }
 
 </style>
