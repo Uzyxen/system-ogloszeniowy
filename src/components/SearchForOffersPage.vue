6bf4a083b95841e06001e06347f44b97b58542c2
Uzyxen <michal912456@gmail.com>
2023-10-14 15:09:07 +0200

    Search for offers page

---
+++ src/components/SearchForOffersPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <article>
+        <h2>Szukanie ofert</h2>
+    </article>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    article{
+        height: 100vh;
+        padding: 0 150px;
+    }
+
+    h2{
+        text-align: center;
+        color: #6244DB;
+        font-size: 30px;
+        font-weight: 700;
+    }
+
+</style>
